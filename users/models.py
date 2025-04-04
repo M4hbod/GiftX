860945302eca025a1a464d89ab6173545400ceeb
M4hbod <74229780+M4hbod@users.noreply.github.com>
2025-04-05 01:32:33 +0330

    fix superuser creation

--- users/models.py
+++ users/models.py
@@ -15,6 +15,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     created_at = models.DateTimeField(auto_now_add=True)
 
     USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["phone_number"]
     objects = CustomUsermanager()
 
     def __str__(self):
